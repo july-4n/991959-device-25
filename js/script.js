659673f6533b4e9d1b5053e85aed4f6df4dcc28f
July-4n <september4n@gmail.com>
2019-07-03 18:55:25 +0500

    Исправление размеров

--- js/script.js
+++ js/script.js
@@ -72,7 +72,7 @@ mapClose.addEventListener("click", function (evt) {
 });
 
 window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode ===27) {
+  if (evt.keyCode === 27) {
     evt.preventDefault ();
     if (mapPopup.classList.contains ("modal-show")) {
       mapPopup.classList.remove("modal-show");
